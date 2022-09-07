e6a03a38110394f76bab4266aebb5275cee4842a
niklas <niklas.d.larsson@gmail.com>
2022-09-07 09:07:47 +0200

    Changing to logger

--- example.py
+++ example.py
@@ -2,9 +2,8 @@ import asyncio
 import logging
 import os
 
-from dotenv import load_dotenv
-
 from adapters.opportunity_adapter import OpportunityAdapter
+from dotenv import load_dotenv
 from models.opportunity import Opportunity
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s")
@@ -17,11 +16,11 @@ async def main():
 	logger.info("Starting Example app")
 	async with OpportunityAdapter(api_key=api_key) as api:
 		example_opp: Opportunity = await api.get_opportunity('ab4e3f06-94b0-41b6-a90e-752a1554acfb')
-		print(example_opp.name)
+		logger.info(example_opp.name)
 
-		params = {'limit': 100, 'archived': 'false', 'stage_id': 'applicant-new'}
+		params = {'limit': 100, 'archived': 'false', 'stage_id': 'lead-new'}
 		opps: list[Opportunity] = await api.get_all_opportunities(params)
-		print(len(opps))
+		logger.info(len(opps))
 
 
 if __name__ == "__main__":
